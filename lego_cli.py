a236937c0673d289417d3a27fefca15575e6286e
Miguel Di Lalla <mdll71421@gmail.com>
2025-03-09 18:11:43 +0000

    Fix syntax error in cleanup command's file check condition

--- lego_cli.py
+++ lego_cli.py
@@ -731,7 +731,7 @@ def cleanup_cmd(all_files, logs_only, cache_only, results_only):
             for item in os.listdir(folder_path):
                 item_path = os.path.join(folder_path, item)
                 try:
-                    if os.path.isfile(item_path)):
+                    if os.path.isfile(item_path):
                         os.unlink(item_path)
                     elif os.path.isdir(item_path):
                         import shutil
